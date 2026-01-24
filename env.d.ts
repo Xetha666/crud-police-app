0e036152f725a9e3c7091bd572252ef438b3359c
Xetha666 <bryans27092000@gmail.com>
2026-01-24 01:26:44 -0500

    Add ImportMetaEnv interface to define environment variables for DB user password and JWT secret

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="astro/client" />
 
+interface ImportMetaEnv {
+  readonly DB_USER_PASSWORD: string;
+  readonly JWT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 interface UserSession {
   id: number;
   username: string;
