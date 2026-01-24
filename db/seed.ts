8cb3399e8e8cae4e4bbfe554e7f0de9db6c26c7c
Xetha666 <bryans27092000@gmail.com>
2026-01-24 01:27:18 -0500

    Hash user password during database seeding and add error handling for missing environment variable

--- db/seed.ts
+++ db/seed.ts
@@ -1,7 +1,24 @@
 import { db, User } from 'astro:db';
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
 
 export default async function seed() {
+  const password = process.env.DB_USER_PASSWORD;
+
+  if (!password) {
+    throw new Error("ERROR: No se encontró DB_USER_PASSWORD en el archivo .env");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  //await db.delete(User);
+
   await db.insert(User).values([
-    { id: "1", username: "Edgard2692", password: "Esaro2692@", name: "Edgard" }
-  ]);	
+    { 
+      id: "1", 
+      username: "Edgard2692", 
+      password: hashedPassword,
+      name: "Edgard" 
+    }
+  ]);
 }
